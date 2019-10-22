6aca792488149dd18b8cb65fc24afd53a1b0a338
Gaston Longhitano <gaston@juntacorp.com>
2019-10-22 14:22:54 -0400

    build commit

---
+++ build/precache-manifest.ae6031d1d54acfc76bd056e41aa8332f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aee0a3e406e02116234ccf03fceb46c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eca899da82be64f3718",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "055ee0546851a7e25ac8",
+    "url": "/static/js/2.88cad625.chunk.js"
+  },
+  {
+    "revision": "9eca899da82be64f3718",
+    "url": "/static/js/main.e00b2b2b.chunk.js"
+  },
+  {
+    "revision": "5a660c721bcbf9dde1cb",
+    "url": "/static/js/runtime-main.c271f4af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
